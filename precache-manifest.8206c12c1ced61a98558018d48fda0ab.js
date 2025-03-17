b6ff5748395ba8ae773ccd844c17e7c18539af65
arielsoothy <ariel.soothy@simplex.com>
2025-03-17 13:32:51 +0200

    Updates

--- precache-manifest.8206c12c1ced61a98558018d48fda0ab.js
+++ precache-manifest.8206c12c1ced61a98558018d48fda0ab.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b86688885e00005b8b66b7baa509cc56",
+    "revision": "98ec97fa6cf6f22891b73f1a3969e9f9",
     "url": "/databricks-comparison/index.html"
   },
   {
-    "revision": "ef6415234b701c5aaadf",
-    "url": "/databricks-comparison/static/css/main.0591f18a.chunk.css"
+    "revision": "f6a93572f25a5ea01e5a",
+    "url": "/databricks-comparison/static/css/main.b861f060.chunk.css"
   },
   {
     "revision": "638e200e69d950262938",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/databricks-comparison/static/js/3.a29efab2.chunk.js"
   },
   {
-    "revision": "ef6415234b701c5aaadf",
-    "url": "/databricks-comparison/static/js/main.e9847f60.chunk.js"
+    "revision": "f6a93572f25a5ea01e5a",
+    "url": "/databricks-comparison/static/js/main.19c1049d.chunk.js"
   },
   {
     "revision": "7ba32071a7bf8fb87583",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/databricks-comparison/precache-manifest.5c4f56e2abdeb85c2bb80dd905653753.js"
+  "/databricks-comparison/precache-manifest.8206c12c1ced61a98558018d48fda0ab.js"
 );
 
 self.addEventListener('message', (event) => {
